9e9f31a2417d1fa08af6fabc006fbe04e29817d2
Sabin Marcu <sabinmarcu@gmail.com>
2015-09-28 22:39:36 +0300

    Added File Explorer Demos

---
+++ file-explorer/v1/0.76893294591ad8542e04.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(0,{
+
+/***/ 443:
+/***/ function(module, exports, __webpack_require__) {
+
+	eval("exports = module.exports = __webpack_require__(389)();\n// imports\n\n\n// module\nexports.push([module.id, \"._3TiqH2zs3W-QrbUtyxXPcN {\\n  display: flex;\\n  flex-flow: row wrap;\\n  align-content: flex-start;\\n  margin: 0;\\n  padding: 30px 10px 0;\\n  height: calc(100% - 30px);\\n  overflow-x: hidden;\\n  overflow-y: auto;\\n}\\n\", \"\"]);\n\n// exports\nexports.locals = {\n\t\"list\": \"_3TiqH2zs3W-QrbUtyxXPcN\"\n};\n\n/*****************\n ** WEBPACK FOOTER\n ** ./~/css-loader?modules!./~/stylus-loader!./src/app/components/ItemsList/style.styl\n ** module id = 443\n ** module chunks = 0\n **/\n//# sourceURL=webpack:///./src/app/components/ItemsList/style.styl?./~/css-loader?modules!./~/stylus-loader");
+
+/***/ }
+
+})
